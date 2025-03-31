8f827f5c4a5c452d4095d2676250dc3a16334dc8
sago-code <139510724+sago-code@users.noreply.github.com>
2025-03-31 16:25:00 -0500

    changes

--- src/Components/Modules/Home.js
+++ src/Components/Modules/Home.js
@@ -13,170 +13,158 @@ import '@splidejs/react-splide/css';
 import '@splidejs/react-splide/css/skyblue';
 import '@splidejs/react-splide/css/sea-green';
 import '@splidejs/react-splide/css/core';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function Home() {
-  const images = [
-    {
-      src: producto1,
-      alt: 'Pantalón 1',
-      title: 'Jeans Azul Casual',
-      oldPrice: '$120.000',
-      newPrice: '$89.900',
-      discount: '25% OFF',
-    },
-    {
-      src: producto2,
-      alt: 'Pantalón 2',
-      title: 'Jeans Negro Casual',
-      oldPrice: '$120.000',
-      newPrice: '$89.900',
-      discount: '25% OFF',
-    },
-    {
-      src: producto3,
-      alt: 'Camiseta 1',
-      title: 'Camiseta sport blanca',
-      newPrice: '$65.900',
-    },
-    {
-      src: producto4,
-      alt: 'Chaqueta 1',
-      title: 'Chaqueta escudería',
-      newPrice: '$90.900',
-    },
-    {
-      src: producto5,
-      alt: 'Conjunto 1',
-      title: 'Conjunto en Rib',
-      newPrice: '$90.900',
-    },
-    {
-      src: producto6,
-      alt: 'Zapatos 1',
-      title: 'Zapatillas Nike Air Force 1',
-      oldPrice: '$210.000',
-      newPrice: '$189.900',
-      discount: '10% OFF',
-    },
-    {
-      src: producto7,
-      alt: 'Pantaloneta 1',
-      title: 'Pantaloneta en tela fria gruesa',
-      newPrice: '$90.900',
-    },
-    {
-      src: producto8,
-      alt: 'Vestido 1',
-      title: 'Vestido en hilo',
-      newPrice: '$210.900',
-    },
-  ];
+    const images = [
+        {
+            src: producto1,
+            alt: 'Pantalón 1',
+            title: 'Jeans Azul Casual',
+            oldPrice: '$120.000',
+            newPrice: '$89.900',
+            discount: '25% OFF',
+            },
+        {
+            src: producto2,
+            alt: 'Pantalón 2',
+            title: 'Jeans Negro Casual',
+            oldPrice: '$120.000',
+            newPrice: '$89.900',
+            discount: '25% OFF',
+        },
+        {
+            src: producto3,
+            alt: 'Camiseta 1',
+            title: 'Camiseta sport blanca',
+            newPrice: '$65.900',
+        },
+        {
+            src: producto4,
+            alt: 'Chaqueta 1',
+            title: 'Chaqueta escudería',
+            newPrice: '$90.900',
+        },
+        {
+            src: producto5,
+            alt: 'Conjunto 1',
+            title: 'Conjunto en Rib',
+            newPrice: '$90.900',
+        },
+        {
+            src: producto6,
+            alt: 'Zapatos 1',
+            title: 'Zapatillas Nike Air Force 1',
+            oldPrice: '$210.000',
+            newPrice: '$189.900',
+            discount: '10% OFF',
+        },
+        {
+            src: producto7,
+            alt: 'Pantaloneta 1',
+            title: 'Pantaloneta en tela fria gruesa',
+            newPrice: '$90.900',
+        },
+        {
+            src: producto8,
+            alt: 'Vestido 1',
+            title: 'Vestido en hilo',
+            newPrice: '$210.900',
+        },
+    ];
 
-  return (
-    <>
-      <Menu />
-      <div className="slidersContainer">
-        {/*Carusel seccion lo mas vendido*/}
-        <Splide
-          options={{
-            type: "loop",
-            autoWidth: true,
-            autoplay: true,
-            arrows: true,
-            pagination: false,
-            rewind: true,
-            gap: '15px',
-            clones: 0,
-          }}
-          hasTrack={false}
-        >
-          <div className="custom-wrapper">
-            <h3>Lo más vendido</h3>
-            <SplideTrack>
-              {images.map((image, index) => (
-                <SplideSlide key={index}>
-                  <div className="product-card">
-                    <img src={image.src} alt={image.alt} className="product-image" />
-                    <div className="product-info">
-                      <h3 className="product-title">{image.title}</h3>
-                      <p className="product-old-price">{image.oldPrice}</p>
-                      <p className="product-new-price">{image.newPrice}</p>
-                      <p className="product-discount">{image.discount}</p>
+    return (
+        <>
+        <Menu />
+        <div className="slidersContainer">
+            {/*Carusel seccion lo mas vendido*/}
+            <Splide
+            options={{
+                type: "loop",
+                autoWidth: true,
+                autoplay: true,
+                arrows: true,
+                navigation: true,
+                pagination: false,
+                rewind: true,
+                gap: '15px',
+                clones: 0,
+            }}
+            hasTrack={false}
+            >
+            <div className="custom-wrapper">
+                <h3>Lo más vendido</h3>
+                <SplideTrack>
+                {images.map((image, index) => (
+                    <SplideSlide key={index}>
+                    <div className="product-card">
+                        <div className="card-background" style={{ backgroundImage: `url(${image.src})` }}>
+                        <div className="product-info">
+                            <h3 className="product-title">{image.title}</h3>
+                            {image.oldPrice && <p className="product-old-price">{image.oldPrice}</p>}
+                            <p className="product-new-price">{image.newPrice}</p>
+                            {image.discount && <span className="discount-badge">{image.discount}</span>}
+                        </div>
+                        </div>
                     </div>
-                  </div>
-                </SplideSlide>
-              ))}
-                <SplideSlide>
-                  <div className="ver-mas-container">
-                    <button className="ver-mas-btn">Ver más</button>
-                  </div>
-                </SplideSlide>
-            </SplideTrack>
-            <div className="splide__arrows">
-                <button className="splide__arrow splide__arrow--prev">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-                    </svg>
-                </button>
-                <button className="splide__arrow splide__arrow--next">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-                    </svg>
-                </button>
-            </div>
-          </div>
-        </Splide>
-        {/*Carusel seccion para ti*/}
-        <Splide
-          options={{
-            type: "loop",
-            autoWidth: true,
-            autoplay: true,
-            arrows: true,
-            pagination: false,
-            rewind: true,
-            gap: '15px',
-            clones: 0,
-          }}
-          hasTrack={false}
-        >
-          <div className="custom-wrapper">
-            <h3>Para ti</h3>
-            <SplideTrack>
-              {images.map((image, index) => (
-                <SplideSlide key={index}>
-                  <div className="product-card">
-                    <img src={image.src} alt={image.alt} className="product-image" />
-                    <div className="product-info">
-                      <h3 className="product-title">{image.title}</h3>
-                      <p className="product-old-price">{image.oldPrice}</p>
-                      <p className="product-new-price">{image.newPrice}</p>
-                      <p className="product-discount">{image.discount}</p>
+                    </SplideSlide>
+                ))}
+                    <SplideSlide>
+                    <div className="ver-mas-container">
+                        <button className="btn btn-primary btn-lg">
+                            Ver más
+                            <i className="bi bi-arrow-right ms-2"></i>
+                        </button>
                     </div>
-                  </div>
-                </SplideSlide>
-              ))}
-                <SplideSlide>
-                  <div className="ver-mas-container">
-                    <button className="ver-mas-btn">Ver más</button>
-                  </div>
-                </SplideSlide>
-            </SplideTrack>
-            <div className="splide__arrows">
-                <button className="splide__arrow splide__arrow--prev">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-                    </svg>
-                </button>
-                <button className="splide__arrow splide__arrow--next">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-                    </svg>
-                </button>
+                    </SplideSlide>
+                </SplideTrack>
             </div>
-          </div>
-        </Splide>
-      </div>
-    </>
-  );
+            </Splide>
+            {/*Carusel seccion para ti*/}
+            <Splide
+            options={{
+                type: "loop",
+                autoWidth: true,
+                autoplay: true,
+                arrows: true,
+                navigation: true,
+                pagination: false,
+                rewind: true,
+                gap: '15px',
+                clones: 0,
+            }}
+            hasTrack={false}
+            >
+            <div className="custom-wrapper">
+                <h3>Para ti</h3>
+                <SplideTrack>
+                {images.map((image, index) => (
+                    <SplideSlide key={index}>
+                    <div className="product-card">
+                        <div className="card-background" style={{ backgroundImage: `url(${image.src})` }}>
+                        <div className="product-info">
+                            <h3 className="product-title">{image.title}</h3>
+                            {image.oldPrice && <p className="product-old-price">{image.oldPrice}</p>}
+                            <p className="product-new-price">{image.newPrice}</p>
+                            {image.discount && <span className="discount-badge">{image.discount}</span>}
+                        </div>
+                        </div>
+                    </div>
+                    </SplideSlide>
+                ))}
+                    <SplideSlide>
+                    <div className="ver-mas-container">
+                        <button className="btn btn-primary btn-lg">
+                            Ver más
+                            <i className="bi bi-arrow-right ms-2"></i>
+                        </button>
+                    </div>
+                    </SplideSlide>
+                </SplideTrack>
+            </div>
+            </Splide>
+        </div>
+        </>
+    );
 }
